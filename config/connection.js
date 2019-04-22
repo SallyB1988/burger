7a86858b1d6393e97fa62c4526f023d4885da9df
Sally Beckwith <sallypbeckwith@gmail.com>
2019-04-22 09:59:31 -0400

    added heroku jawsdb info

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,17 @@
 const mysql = require('mysql');
-
+var connection;
 // connection to mysql database
-var connection = mysql.createConnection({
-  host: 'localhost',
-  port: process.env.PORT || 3306, // SALLY -- Is this correct?
-  user: 'root',
-  password: 'root',     // Do I have to show everyone my password?
-  database: 'burgers_db'
-});
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL)
+  } else {
+      connection = mysql.createConnection({
+      host: 'localhost',
+      port: 3306, // SALLY -- Is this correct?
+      user: 'root',
+      password: 'root',     // Do I have to show everyone my password?
+      database: 'burgers_db'
+    });
+  }
 
 connection.connect(function(err) {
   if (err) throw err;
